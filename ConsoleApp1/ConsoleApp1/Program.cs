3c5292b1cceb76205c906e85f7b5c09e79bee163
Leena <leena4talk@gmail.com>
2022-08-07 23:58:43 +0530

    Adding ConsoleApp1 and ConsoleApp3

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+/*class Test
+{
+    /*static void Main(string[] args)
+    {
+        double result = GetPower(6.5, 2.5);
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+
+    static void CreateTestPackage()
+    { 
+       
+    }
+
+
+
+        
+    static double GetPower(double baseNumber, double powerNumber)
+        {
+            double result = 1;
+            for(int i=0; i<powerNumber; i++)
+            {
+                result = result* baseNumber;
+            }   
+            return result;
+        }
+}*/
